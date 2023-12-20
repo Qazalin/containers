76a2b5e6a5726789778daf2da6034ffb8443c01c
qazal <qazal.software@gmail.com>
2023-12-20 13:12:18 +0200

    new dockerfile dev env

--- cuda-ci-old/README.md
+++
@@ -1,3 +0,0 @@
-run tinygrad CUDA ci on a mac:
-
-`./setup.sh && docker run -it --rm tinygrad_cuda /bin/bash`
--- cuda-ci-old/setup.sh
+++
@@ -1,6 +0,0 @@
-start_dir=$(pwd)
-cd /Users/qazal/code/tinygrad/tinygrad
-git ls-files | tar -czf $start_dir/tinygrad.tar.gz -T -
-cd $start_dir
-docker build -t tinygrad_cuda .
-rm tinygrad.tar.gz
